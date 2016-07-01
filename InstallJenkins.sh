3ce00dd6f8fe9e3b8427b58f473eb9a893c04fce
Jeffrey Cañas <jeffcanas13@users.noreply.github.com>
2016-07-02 06:22:08 +0800

    Update InstallJenkins.sh

--- InstallJenkins.sh
+++ InstallJenkins.sh
@@ -1,5 +1,31 @@
-apt-get update  
-    wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-    echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
-    apt-get update
-    apt-get install jenkins
+#!/bin/bash
+OPTIONS="LTS Weekly_Build"
+select opt in $OPTIONS; do
+        if [ "$opt" = "LTS" ]; then
+           echo "You have chosen Jenkins LTS"
+           sleep 3
+           apt-get update
+           wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+           echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list
+           apt-get update
+           apt-get install jenkins
+           echo "Successfully Installed Jenkins LTS"
+           exit
+        elif [ "$opt" = "Weekly_Build" ]; then
+           echo "You have chosen Jenkins Weekly Build"
+           sleep 3
+           apt-get update
+           wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+           echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+           apt-get update
+           apt-get install jenkins
+           echo "Successfully Installed Jenkins Weekly Build"
+           exit
+        else
+           clear
+           echo Bad Option choose only LTS or Weekly_Build. Please run it again.
+           exit
+        fi
+done
+
+exit
